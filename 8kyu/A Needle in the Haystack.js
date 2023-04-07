d2ebd134028ab40357154af560adaca080f05bcf
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-04-07 14:06:56 -0700

    Added "A Needle in the Haystack" kata

---
+++ 8kyu/A Needle in the Haystack.js
@@ -0,0 +1,26 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// Note: In COBOL, it should return "found the needle at position 6"
+
+
+function findNeedle(haystack) {
+    for (let i = 0; i < haystack.length; i++){
+        if (haystack[i] === "needle"){
+            return `found the needle at position ${i}`
+        }
+    }
+}
+
+// function findNeedle(haystack) {
+//     return `found the needle at position ${haystack.indexOf("needle")}`
+            // indexOf() method will return the first index at which a given element is found or -1 if it is not found
+// }
