f0bb3183c885ed44b7d0b8419abf2a347bca8488
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-05-01 21:17:19 -0700

    Added "Function 2 - squaring an argument" kata

---
+++ 8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,12 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+
+function square(num) {
+    // Takes num and increases it to the power of 2
+    return Math.pow(num, 2)
+
+    // Alternatives
+    // return num * num
+    // return num ** 2
+}
