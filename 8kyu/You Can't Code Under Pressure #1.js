afbc88471b3b0b1fc4b74ffbb276f27fc7699465
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-05-01 21:19:02 -0700

    Added "You Can't Code Under Pressure #1" kata

---
+++ 8kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+
+
+function doubleInteger(i) {
+    return i * 2
+    
+    // Alternative
+    // i *= 2
+    // return i
+}
