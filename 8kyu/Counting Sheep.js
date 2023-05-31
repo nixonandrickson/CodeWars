49ea2af2dfc9bc249e849f47a3e3823b1f6143a2
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-05-31 11:49:47 -0700

    Added "Couning Sheep" kata

---
+++ 8kyu/Counting Sheep.js
@@ -0,0 +1,17 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+
+function countSheeps(arrayOfSheep) {
+    //Filters out any value that isn't true
+    return arrayOfSheep.filter(Boolean).length
+}
