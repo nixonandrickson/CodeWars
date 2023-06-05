ed787f68640a1d943307a23d48ee7fe047b63cab
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-06-05 10:54:09 -0700

    Added "Sum of all Arguments" kata

---
+++ 7kyu/Sum of all Arguments.js
@@ -0,0 +1,18 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+
+
+function sum(n) {
+    let sum = 0 
+
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+     return sum
+}
