e7a03b2d6f045367530c71fff0524c79dfc71554
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-09-14 12:16:58 -0700

    Added "Get Ascii Value of Character" kata

---
+++ 8kyu/get ascii value of character.js
@@ -0,0 +1,8 @@
+// Get ASCII value of a character.
+
+
+
+function getASCII(c) {
+    // The charCodeAt() method of String values returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index
+    return c.charCodeAt(0)
+}
