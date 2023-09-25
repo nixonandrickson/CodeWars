1c1e645294e7e6c5fa201e433859ff8019997e17
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-09-25 11:27:03 -0700

    Added "MakeUpperCase" kata

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+
+
+
+function makeUpperCase(str) {
+    // The .toUpperCase() method is used to make any string all uppercase. 
+    return str.toUpperCase()
+}
