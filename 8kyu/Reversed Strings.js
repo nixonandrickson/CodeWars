35ee26c3945756ca1e4fc0edfd283362dc619d84
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-10-26 15:16:40 -0700

    Added comments for "Reverse Strings" kata

--- 8kyu/Reversed Strings.js
+++ 8kyu/Reversed Strings.js
@@ -5,8 +5,8 @@
 
 
 function solution(str) {
+    // .split('') method takes the string and turns it into an array. Each character having it's own index in the array
+    // .reverse() method reverses the order of the array
+    // .join('') method puts all the indexes of the array into a string with no spaces inbetween characters
     return str.split('').reverse().join('')
 }
-
-console.log(solution('world'))
-console.log(solution('word'))
